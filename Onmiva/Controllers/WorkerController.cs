2aeae0a200b471920d3845285d5c4d09a996da96
FrozenCamel <martynas.greicius@gmail.com>
2020-11-10 15:48:54 +0200

    bandau prideti darbuotojus

---
+++ Onmiva/Controllers/WorkerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Onmiva.Controllers
+{
+    public class WorkerControler:Controller
+    {
+        public ActionResult Workers()
+        {
+            return View();
+        }
+        public ActionResult CreateWork()
+        {
+            return View();
+        }
+        public ActionResult WrokerAndWork()
+        {
+            return View();
+        }
+
+    }
+}
