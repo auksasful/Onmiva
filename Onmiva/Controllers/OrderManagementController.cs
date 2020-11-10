d24d1df421e3145da7b6ceae986a2da79d17b354
auksasful <akurat2012@gmail.com>
2020-11-10 14:37:54 +0200

    Fix after commits

---
+++ Onmiva/Controllers/OrderManagementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Onmiva.Controllers
+{
+    public class OrderManagementController : Controller
+    {
+        // GET: OrderManagement
+        public ActionResult OrderCreate()
+        {
+            return View();
+        }
+
+        public ActionResult OrderEdit()
+        {
+            return View();
+        }
+
+        public ActionResult OrdersList()
+        {
+            return View();
+        }
+
+        public ActionResult OrderView()
+        {
+            return View();
+        }
+
+        public ActionResult FilterOrders()
+        {
+            return View();
+        }
+    }
+}
